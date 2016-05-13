c63ad0ee78739fcd3c0af744ee5b75642e1b1dca
Kate Sowles <katesowles@gmail.com>
2016-05-13 14:53:11 -0700

    rough start

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-#!/usr/bin/env node
+const fs = require('fs');
+var palette = 'palette-bitmap.bmp';
+// var nonpalette = 'non-palette-bitmap.bmp';
+
+fs.readFile(palette,(err,buffer) => {
+  const bitmap = new Bitmap(buffer);
+  console.log(bitmap.offset);
+});
+
+function Bitmap(buffer) {
+  this.header = buffer.readInt32LE(0);
+  this.offset = buffer.readInt32LE(10);
+  this.headerSize = buffer.readUInt32LE(14);
+  this.bitmapWidth = buffer.readUInt32LE(18);
+  this.bitmapHeight = buffer.readUInt32LE(22);
+  this.colorPlanes = buffer.readUInt16LE(26);
+  this.bitsPerPixel = buffer.readUInt16LE(28);
+  this.compression = buffer.readUInt32LE(30);
+  this.imageSize = buffer.readUInt32LE(34);
+  this.horizontalRes = buffer.readUInt32LE(38);
+  this.verticalRes = buffer.readUInt32LE(42);
+  this.numColorsPalette = buffer.readUInt32LE(46);
+  this.numImportColors = buffer.readUInt32LE(50);
+}
+
+function invertColors() {
+
+}
+
+function alterBitmap(originalFile, newFile) {
+  var readFile = fs.readFile(palette);
+  // var alteredBitmap;  // waiting to be set
+  readFile.on('data', function(data) {
+    var headerInfo = new Bitmap(data);
+    if (headerInfo.header !== 'BM' || headerInfo.headerSize !== 40) { // is not proper bmp
+      console.log('not a proper bitmap');
+    }
+    else {  // is proper bitmap
+      console.log('is a proper bitmap');
+
+    }
+  });
+}
---
+++ test.index.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var fs = require('fs');
+
+describe('testing color transforms of bitmap image -', function() {
+  it('is a valid bitmap?', function() {
+    
+  });
+  it('has the data been altered?', function() {
+
+  });
+  it('has proper palette offset?', function() {
+
+  });
+});

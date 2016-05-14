aaadae2fa849ac80d2662b01e8fa01b46c920156
Yvonne Hayes <queyvonne@gmail.com>
2016-05-13 17:00:44 -0700

    test for comparing two files works perfectly.

--- test/transform2.test.js
+++ test/transform2.test.js
@@ -7,6 +7,6 @@ var fileTwo = fs.readFileSync('./palette-bitmapCOPY.bmp');
 describe( 'comparing files to see if match', () => {
 
   it ('palette-bitmap.bmp should match palette-bitmapCOPY.bmp', () => {
-    assert.Equal(fileOne,fileTwo);
+    assert.deepEqual(fileOne,fileTwo);
   });
 });
